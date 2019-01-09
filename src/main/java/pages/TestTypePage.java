a6241ab8666ed53c0f02ff19fd61cb546d187906
Teodor Cosmin Alexandru <talexandru@deloittece.com>
2019-01-09 18:33:20 +0200

    CVSB-145 Implemented the 3 test cases for remove defect

--- src/main/java/pages/TestTypePage.java
+++ src/main/java/pages/TestTypePage.java
@@ -1,12 +1,22 @@
 package pages;
 
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestTypePage extends BasePage {
 
     private static final String VEHICLE_DETAILS = "Vehicle details";
     private static final String ADD_DEFECT_ID = "Add defect add circle-outline";
+    private static final String REMOVE_BUTTON_ID = "Remove";
+    private static final String POP_UP_CANCEL_ID = "Cancel";
+    private static final String POP_UP_REMOVE_ID = "Remove";
+    private static final String POP_UP_TEXT_ID = "This action wil remove this defect.";
+    private static final String POP_UP_TITLE_ID = "Remove defect";
 
 
     public int checkResultIsPresent(String result) {
@@ -31,6 +41,18 @@ public class TestTypePage extends BasePage {
         return findElementByXpath("//*[contains(@label, '" + defect + "')]");
     }
 
+    public boolean isDefectVisible(String defect) {
+        WebElement defectElement = null;
+        try {
+            defectElement = findElementById(defect);
+        } catch (Exception e) {
+        }
+        if (defectElement != null) {
+            return defectElement.isDisplayed();
+        } else
+            return false;
+    }
+
     public WebElement getElementById(String id) {
         return findElementById(id);
     }
@@ -49,4 +71,36 @@ public class TestTypePage extends BasePage {
         }
     }
 
+    public void swipeLeftOnDefect(String defectId) {
+        WebElement defectToSwipe = findElementById(defectId);
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        Map<String, Object> params = new HashMap<>();
+        params.put("direction", "left");
+        params.put("element", ((RemoteWebElement) defectToSwipe).getId());
+        js.executeScript("mobile: swipe", params);
+    }
+
+    public void clickOnRemoveButton() {
+        findElementById(REMOVE_BUTTON_ID).click();
+    }
+
+    public boolean isDefectRemovalPopUpVisible() {
+        boolean status = false;
+        WebElement cancelButton = findElementById(POP_UP_CANCEL_ID);
+        WebElement removeButton = findElementById(POP_UP_REMOVE_ID);
+        WebElement text = findElementById(POP_UP_TEXT_ID);
+        WebElement title = findElementById(POP_UP_TITLE_ID);
+        if (cancelButton.isDisplayed() && removeButton.isDisplayed() && text.isDisplayed() && title.isDisplayed()) {
+            status = true;
+        }
+        return status;
+    }
+
+    public void clickRemoveInPopUp() {
+        findElementById(POP_UP_REMOVE_ID).click();
+    }
+
+    public void clickCancelInPopUp() {
+        findElementById(POP_UP_CANCEL_ID).click();
+    }
 }
