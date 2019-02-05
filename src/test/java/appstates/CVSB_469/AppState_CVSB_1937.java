f39dd66a3f2fed6641ee3d2e4257831fc6ad3ec9
George Laurentiu Vasile <gvasile@deloittece.com>
2019-02-05 11:11:13 +0200

    Added rest of automation for application state

---
+++ src/test/java/appstates/CVSB_469/AppState_CVSB_1937.java
@@ -0,0 +1,36 @@
+package appstates.CVSB_469;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import steps.*;
+import util.BaseTestClass;
+
+@RunWith(SerenityRunner.class)
+public class AppState_CVSB_1937 extends BaseTestClass {
+
+    @Steps
+    LaunchSteps launchSteps;
+
+    @Steps
+    CommonSteps commonSteps;
+
+    @Steps
+    SearchForAnATFSteps searchForAnATFSteps;
+
+
+    @Title("CVSB-469 - AC2 Reopen the app after it was closed - redirected to the homepage screen (ex. app closed by the user, app closed by the device, device turned off)")
+    @Test
+    public void testAppStateCloseAtSearchForAtfPage() {
+        launchSteps.clickGetStarted();
+        searchForAnATFSteps.waitForPageToLoadAndSelectAnAtf(atfService.getUniqueIdentifier(0));
+        commonSteps.getPage().closeAndLaunchApp();
+        launchSteps.clickGetStarted();
+        searchForAnATFSteps.waitForPageToLoadAndSelectAnAtf(atfService.getUniqueIdentifier(0));
+
+    }
+
+
+}
