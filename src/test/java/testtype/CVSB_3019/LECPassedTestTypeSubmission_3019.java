e04f4ac68d406a617927de73efc6468c91dad5ef
Dorin Nicolae <dnicolae@deloittece.com>
2019-04-02 10:34:46 +0300

    feature/CVSB-3019

---
+++ src/test/java/testtype/CVSB_3019/LECPassedTestTypeSubmission_3019.java
@@ -0,0 +1,59 @@
+package testtype.CVSB_3019;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import pages.TestPage;
+import steps.*;
+import steps.composed.TestTypeCategoryComp;
+import util.BaseTestClass;
+
+@RunWith(SerenityRunner.class)
+public class LECPassedTestTypeSubmission_3019 extends BaseTestClass {
+
+    @Steps
+    TestTypeCategoryComp testTypeCategoryComp;
+
+    @Steps
+    TestSteps testSteps;
+
+    @Steps
+    TestTypeCategorySteps testTypeCategorySteps;
+
+    @Steps
+    TestTypeDetailsSteps testTypeDetailsSteps;
+
+    @Steps
+    EUVehicleCategorySteps euVehicleCategorySteps;
+
+    @Steps
+    OdometerReadingSteps odometerReadingSteps;
+
+    @Steps
+    TestReviewSteps testReviewSteps;
+
+    @Steps
+    SiteVisitSteps siteVisitSteps;
+
+    @Title("CVSB-3019 - Defect - Technical LEC submission")
+    @Test
+    public void submitPassedLECTechnicalTest() {
+        testTypeCategoryComp.goToTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName());        testTypeCategorySteps.selectFromTestTypeList("Technical test");
+        testTypeCategorySteps.selectFromTestTypeList("Technical test");
+        testTypeCategorySteps.selectFromTestTypeList("LEC");
+        testSteps.selectTestType("Technical test", TestPage.TestTypeStatuses.IN_PROGRESS);
+        testTypeDetailsSteps.setTestToOption("Pass");
+        testTypeDetailsSteps.sendCertificateNumber("12345567");
+        testSteps.selectVehicleCategoryOption();
+        euVehicleCategorySteps.selectM1Option();
+        testSteps.selectOdometerReading();
+        odometerReadingSteps.typeInField("123");
+        odometerReadingSteps.pressSave();
+        testSteps.reviewAction();
+        testReviewSteps.pressSubmit();
+        testReviewSteps.pressSubmitInPopUp();
+        siteVisitSteps.checkSiteVisitPage();
+    }
+}
