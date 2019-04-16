1f423c18ea390585cb70dfb2dd1fcfcac14ff7c4
Dorin Nicolae <dnicolae@deloittece.com>
2019-04-16 11:23:17 +0300

    CVSB_972 test

---
+++ src/test/java/defect/CVSB_972/AddsMinorDefect_CVSB_4380.java
@@ -0,0 +1,52 @@
+package defect.CVSB_972;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import pages.TestPage;
+import steps.*;
+import steps.composed.TestTypeCategoryComp;
+import util.BaseTestClass;
+
+@RunWith(SerenityRunner.class)
+public class AddsMinorDefect_CVSB_4380 extends BaseTestClass {
+
+    @Steps
+    TestTypeCategoryComp testTypeCategoryComp;
+
+    @Steps
+    TestTypeDetailsSteps testTypeDetailsSteps;
+
+    @Steps
+    TestSteps testSteps;
+
+    @Steps
+    DefectCategorySteps defectCategorySteps;
+
+    @Steps
+    DefectItemSteps defectItemSteps;
+
+    @Steps
+    DefectDescriptionSteps defectDescriptionSteps;
+
+    @Steps
+    DefectDetailsSteps defectDetailsSteps;
+
+
+    @Title("CVSB-972 - AC1 - VSA adds a minor defect")
+    @Test
+    public void addsAMinorDefect() {
+        testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName());
+        testSteps.selectTestType("Annual test", TestPage.TestTypeStatuses.IN_PROGRESS);
+        testTypeDetailsSteps.selectAddDefect("Annual test");
+        defectCategorySteps.searchForDefect("59");
+        defectCategorySteps.selectDefectFromList("59. Brake Systems and Components");
+        defectItemSteps.selectDefectFromList("4. Reservoir:");
+        defectDescriptionSteps.selectDefect("59.4 (a) (i) MINOR");
+        defectDetailsSteps.selectOptionsAndTapAddDefect(defectDetailsSteps.inputBrakeSystemMinorDefect());
+        testTypeDetailsSteps.pressSave();
+        defectDetailsSteps.checkPRSOptionIsNotDisplayed();
+    }
+}
