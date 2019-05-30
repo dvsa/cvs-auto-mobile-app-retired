367e5634c02e5b6c34c8594b8f7d3d76cb986590
Dorin Nicolae <dnicolae@deloittece.com>
2019-05-30 16:21:40 +0300

    CVSB-444 first set of tests

---
+++ src/test/java/testtype/CVSB_444/TestTypeLinked_CVSB_4521.java
@@ -0,0 +1,34 @@
+package testtype.CVSB_444;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import steps.TestSteps;
+import steps.TestTypeCategorySteps;
+import steps.composed.TestTypeCategoryComp;
+import util.BaseTestClass;
+
+@RunWith(SerenityRunner.class)
+public class TestTypeLinked_CVSB_4521 extends BaseTestClass {
+
+    @Steps
+    TestSteps testSteps;
+
+    @Steps
+    TestTypeCategorySteps testTypeCategorySteps;
+
+    @Steps
+    TestTypeCategoryComp testTypeCategoryComp;
+
+    @Test
+    @Title("CVSB-444 - TCD - AC1 As a VSA I would like to view only relevant linked tests so that I don't have to go through the full test type taxonomy (Prohibition Clearance Any PSV Full Inspec and Fee with Cert)")
+    public void testListFromAddLinkedTestType() {
+        testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(), "Prohibition Clearance");
+        testTypeCategorySteps.selectFromTestTypeList("Any PSV");
+        testTypeCategorySteps.selectFromTestTypeList("Full Inspection/ Full Fee - with Certificate");
+        testSteps.addLinkedTestType();
+        testTypeCategorySteps.checkTestTypeListHasOnlySomeTestTypes( "Technical test");
+    }
+}
