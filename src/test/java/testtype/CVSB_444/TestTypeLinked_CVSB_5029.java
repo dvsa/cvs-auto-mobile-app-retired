088066285d40794f20324bea23062c267983702a
Dorin Nicolae <dnicolae@deloittece.com>
2019-06-05 15:18:17 +0300

    Added remaining tests for CVSB-444

---
+++ src/test/java/testtype/CVSB_444/TestTypeLinked_CVSB_5029.java
@@ -0,0 +1,35 @@
+package testtype.CVSB_444;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import steps.TestSteps;
+import steps.TestTypeCategorySteps;
+import steps.composed.TestTypeCategoryComp;
+import util.BaseTestClass;
+
+@RunWith(SerenityRunner.class)
+public class TestTypeLinked_CVSB_5029 extends BaseTestClass {
+
+    @Steps
+    TestSteps testSteps;
+
+    @Steps
+    TestTypeCategorySteps testTypeCategorySteps;
+
+    @Steps
+    TestTypeCategoryComp testTypeCategoryComp;
+
+    @Test
+    @Title("CVSB-444 - TCD - AC1 As a VSA I would like to view only relevant linked tests so that I don't have to go through the full test type taxonomy (Paid Retest Any PSV)")
+    public void testListFromAddLinkedTestType() {
+        testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(), "Retest");
+        testTypeCategorySteps.selectFromTestTypeList("Paid");
+        testTypeCategorySteps.selectFromTestTypeList("Any PSV");
+        testSteps.addLinkedTestType();
+        testTypeCategorySteps.checkTestTypeListHasOnlySomeTestTypes("Technical test");
+        testTypeCategorySteps.selectFromTestTypeList("Technical test");
+    }
+}
