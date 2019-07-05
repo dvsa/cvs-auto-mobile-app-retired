a20b48e598aa9712c1d072417d21d2769b1f77d8
Dorin Nicolae <dnicolae@deloittece.com>
2019-07-05 11:36:40 +0300

    Renaming methods and added negative tests for CVSB-3179

--- src/test/java/testtype/CVSB_3179/ProhibitionFieldDisplayed_CVSB_6040.java
+++ src/test/java/testtype/CVSB_3179/ProhibitionFieldDisplayed_CVSB_6040.java
@@ -11,7 +11,7 @@ import steps.composed.TestTypeCategoryComp;
 import util.BaseTestClass;
 
 @RunWith(SerenityRunner.class)
-public class ProhibitionFieldNotDisplayed_CVSB_4829_6 extends BaseTestClass {
+public class ProhibitionFieldDisplayed_CVSB_6040 extends BaseTestClass {
 
     @Steps
     TestSteps testSteps;
@@ -37,21 +37,17 @@ public class ProhibitionFieldNotDisplayed_CVSB_4829_6 extends BaseTestClass {
     @Steps
     DefectDetailsSteps defectDetailsSteps;
 
-    @Title("CVSB-3179 - TCD - AC1 - Prohibition field not displayed at a defect level (Prohibition Clearance)")
+    @Title("CVSB-3179 - TCD - Prohibition field is displayed for dangerous defect (Annual test)")
     @Test
-    public void prohibitionFieldNotDisplayedAtADefectLevelProhibitionClearencePartPaidWithCert() {
-        testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(), "Prohibition Clearance");
-        testTypeCategorySteps.selectFromTestTypeList("Any PSV");
-        testTypeCategorySteps.selectFromTestTypeList("PG9 Retest");
-        testTypeCategorySteps.selectFromTestTypeList("Part Paid");
-        testTypeCategorySteps.selectFromTestTypeList("With certification");
-        testSteps.selectTestType("Prohibition clearance", TestPage.TestTypeStatuses.IN_PROGRESS);
+    public void prohibitionFieldNotDisplayedAtADefectLevelAnnualTest() {
+        testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(), "Annual test");
+        testSteps.selectTestType("Annual test", TestPage.TestTypeStatuses.IN_PROGRESS);
         testTypeDetailsSteps.clickAddDefect();
         defectCategorySteps.selectDefectFromList("8. Condition of Tyres");
         defectItemSteps.selectDefectFromList("1. A tyre");
         defectDescriptionSteps.selectDefect("8.1 (b) DANGEROUS");
-        defectDetailsSteps.checkIssuedInMobileComplianceIsNotPresent();
-        defectDetailsSteps.checkProhibitionLabelIsNotDisplayed();
+        defectDetailsSteps.checkIssuedInMobileComplianceIsPresent();
+        defectDetailsSteps.checkProhibitionLabelIsDisplayed();
         defectDetailsSteps.tapDone();
     }
 }
--- src/test/java/testtype/CVSB_3179/ProhibitionFieldNotDisplayed_CVSB_4829_4.java
+++ src/test/java/testtype/CVSB_3179/ProhibitionFieldNotDisplayed_CVSB_4829_4.java
@@ -40,7 +40,7 @@ public class ProhibitionFieldNotDisplayed_CVSB_4829_4 extends BaseTestClass {
 
     @Title("CVSB-3179 - TCD - AC1 - Prohibition field not displayed at a defect level (Prohibition Clearance)")
     @Test
-    public void prohibitionFieldNotDisplayedAtADefectLevelProhibitionClearence() {
+    public void prohibitionFieldNotDisplayedAtADefectLevelProhibitionClearencePartInspectionPartFee() {
         testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(), "Prohibition Clearance");
         testTypeCategorySteps.selectFromTestTypeList("Any PSV");
         testTypeCategorySteps.selectFromTestTypeList("Part inspection/ part fee");
