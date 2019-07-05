012cefda5e72c4c4a11ce0735f98d3883bf4ae0d
Dorin Nicolae <dnicolae@deloittece.com>
2019-07-05 13:38:47 +0300

    Added CVSB 5139 tests

---
+++ src/test/java/testtype/CVSB_5139/PRSDisplayedForDangerousDefects_CVSB_6351.java
@@ -0,0 +1,49 @@
+package testtype.CVSB_5139;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import pages.TestPage;
+import steps.*;
+import steps.composed.TestTypeCategoryComp;
+import util.BaseTestClass;
+
+@RunWith(SerenityRunner.class)
+public class PRSDisplayedForDangerousDefects_CVSB_6351 extends BaseTestClass {
+
+    @Steps
+    TestSteps testSteps;
+
+    @Steps
+    TestTypeCategoryComp testTypeCategoryComp;
+
+    @Steps
+    TestTypeDetailsSteps testTypeDetailsSteps;
+
+    @Steps
+    DefectCategorySteps defectCategorySteps;
+
+    @Steps
+    DefectItemSteps defectItemSteps;
+
+    @Steps
+    DefectDescriptionSteps defectDescriptionSteps;
+
+    @Steps
+    DefectDetailsSteps defectDetailsSteps;
+
+    @Title("CVSB-5139 - TCD - AC1 - VSA adds a dangerous defect")
+    @Test
+    public void prohibitionFieldNotDisplayedAtADefectLevelAnnualTest() {
+        testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(), "Annual test");
+        testSteps.selectTestType("Annual test", TestPage.TestTypeStatuses.IN_PROGRESS);
+        testTypeDetailsSteps.clickAddDefect();
+        defectCategorySteps.selectDefectFromList("8. Condition of Tyres");
+        defectItemSteps.selectDefectFromList("1. A tyre");
+        defectDescriptionSteps.selectDefect("8.1 (b) DANGEROUS");
+        defectDetailsSteps.setPRS();
+        defectDetailsSteps.tapDone();
+    }
+}
