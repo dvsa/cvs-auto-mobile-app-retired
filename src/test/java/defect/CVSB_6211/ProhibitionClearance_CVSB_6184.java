40f3f557958f941f7d25aa84d017a01bd9ff76da
Alexandru <talexandru@deloittece.com>
2019-07-09 17:05:34 +0300

    CVSB-6184 - Implemented test case

---
+++ src/test/java/defect/CVSB_6211/ProhibitionClearance_CVSB_6184.java
@@ -0,0 +1,59 @@
+package defect.CVSB_6211;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import pages.TestPage;
+import steps.*;
+import steps.composed.TestTypeCategoryComp;
+import util.BaseTestClass;
+
+@RunWith(SerenityRunner.class)
+public class ProhibitionClearance_CVSB_6184 extends BaseTestClass {
+
+    @Steps
+    TestTypeCategoryComp testTypeCategoryComp;
+
+    @Steps
+    TestTypeDetailsSteps testTypeDetailsSteps;
+
+    @Steps
+    TestSteps testSteps;
+
+    @Steps
+    DefectCategorySteps defectCategorySteps;
+
+    @Steps
+    DefectItemSteps defectItemSteps;
+
+    @Steps
+    DefectDescriptionSteps defectDescriptionSteps;
+
+    @Steps
+    DefectDetailsSteps defectDetailsSteps;
+
+    @Steps
+    TestTypeSubcategorySteps testTypeSubcategorySteps;
+
+
+    @Title("CVSB-6184 - Bypass the prohibition raising for Prohibition Clearance tests")
+    @Test
+    public void testBypassProhibitionClearance() {
+        testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(), "Prohibition clearance");
+        testTypeSubcategorySteps.selectFromTestTypeList("Any PSV");
+        testTypeSubcategorySteps.selectFromTestTypeList("PG9 retest");
+        testTypeSubcategorySteps.selectFromTestTypeList("Paid");
+        testTypeSubcategorySteps.selectFromTestTypeList("With certification");
+        testSteps.selectTestType("Prohibition clearance", TestPage.TestTypeStatuses.IN_PROGRESS);
+        testTypeDetailsSteps.selectAddDefect("Paid prohibition clearance (retest with certificate)");
+        defectCategorySteps.searchForDefect("59");
+        defectCategorySteps.selectDefectFromList("59. Brake Systems and Components");
+        defectItemSteps.selectDefectFromList("2. Brake pipes and flexible hoses");
+        defectDescriptionSteps.selectDefect("59.2 (b) (iii) DANGEROUS");
+        defectDetailsSteps.tapDone();
+        testTypeDetailsSteps.checkDefectsArePresentForTest("Paid prohibition clearance (retest with certificate)",
+                "59.2 (b) (iii) DANGEROUS", "2. Brake pipes and flexible hoses");
+    }
+}
