9d31416fd89e09c6a28d9adb6910f3854a1b6e4f
bflorea <bflorea@deloittece.com>
2019-07-25 12:32:04 +0300

    added more tests for CVSB-3904

--- src/test/java/testtype/CVSB_3904/HgvTrlTestTypes_6175.java
+++ src/test/java/testtype/CVSB_3904/HgvTrlTestTypes_6175.java
@@ -4,6 +4,7 @@ import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.annotations.Title;
 import net.thucydides.core.annotations.WithTag;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import pages.TestPage;
