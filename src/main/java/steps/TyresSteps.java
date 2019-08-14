690e5e06e0aaec16f5180bb6e64fef3675488e44
bflorea <bflorea@deloittece.com>
2019-08-14 13:59:43 +0300

    added test for CVSB-6170

--- src/main/java/steps/TyresSteps.java
+++ src/main/java/steps/TyresSteps.java
@@ -1,12 +1,13 @@
 package steps;
 
+import model.vehicles.Weights;
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
-import org.openqa.selenium.WebElement;
 import pages.TyresPage;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,23 +16,12 @@ public class TyresSteps extends ScenarioSteps {
     TyresPage tyresPage;
 
     @Step
-    public void checkDetails(String... details) {
+    public void checkDetails(String tyreSize, String ply, String fitment, String sdIndex, String speedCat,
+                             String tyreCode) {
         tyresPage.waitUntilPageIsLoaded();
         List<String> axleData = tyresPage.getDataSetBetweenTitles("AXLE 1", "");
-        assertThat(axleData).contains(details);
-    }
+        assertThat(axleData).contains(tyreSize, ply, fitment, sdIndex, speedCat, tyreCode);
 
-    @Step
-    public void checkDetailsOfAxle(String axle, String... details) {
-        tyresPage.waitUntilPageIsLoaded();
-        List<WebElement> axleData = tyresPage.getAxleData(axle);
-        List<WebElement> extractedValues = axleData.stream().filter(detail -> axleData.indexOf(detail)%2==0).collect(Collectors.toList());
-        System.out.println("AXLE: " + axle);
-        System.out.println("List of axleData: " + axleData);
-        System.out.println("details: " + Arrays.asList(details));
-        List<String> valuesToString = extractedValues.stream().map(value -> value.getAttribute("name")).collect(Collectors.toList());
-        System.out.println("extracted values: " + valuesToString);
-            assertThat(Arrays.asList(details).equals(valuesToString)).isTrue();
     }
 
     @Step
@@ -43,10 +33,5 @@ public class TyresSteps extends ScenarioSteps {
     public void checkIfFieldIsNotEditable(String field) {
         tyresPage.waitUntilPageIsLoaded();
         assertThat(tyresPage.isFieldEditableById(field)).isFalse();
-    }
-
-    @Step
-    public void checkTyreUseCode(String tyreUseCode) {
-        assertThat(tyresPage.getTyreUseCodeValue().getAttribute("name")).isEqualTo(tyreUseCode);
     }
 }
