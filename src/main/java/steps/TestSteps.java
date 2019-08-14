968975ad05bf835b4f5e030ba3e5ab1ae4c33b84
bflorea <bflorea@deloittece.com>
2019-08-14 15:40:15 +0300

    removed unused imports

--- src/main/java/steps/TestSteps.java
+++ src/main/java/steps/TestSteps.java
@@ -4,8 +4,6 @@ import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
 import pages.TestPage;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestSteps extends ScenarioSteps {
