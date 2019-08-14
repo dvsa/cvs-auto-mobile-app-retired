9a6ccac63e175bd8156b17e664cb08b36503a444
bflorea <bflorea@deloittece.com>
2019-08-14 13:59:43 +0300

    added test for CVSB-6170

--- src/main/java/steps/TyresSteps.java
+++ src/main/java/steps/TyresSteps.java
@@ -1,12 +1,12 @@
 package steps;
 
+import model.vehicles.Weights;
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
 import org.openqa.selenium.WebElement;
 import pages.TyresPage;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
