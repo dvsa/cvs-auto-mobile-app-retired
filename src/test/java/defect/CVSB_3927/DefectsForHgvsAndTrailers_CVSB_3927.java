8cdfaaae058f432107921466e197800d3afaedd0
bflorea <bflorea@deloittece.com>
2019-08-20 11:59:26 +0300

    removed unused imports

---
+++ src/test/java/defect/CVSB_3927/DefectsForHgvsAndTrailers_CVSB_3927.java
@@ -0,0 +1,9 @@
+package defect.CVSB_3927;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.junit.runner.RunWith;
+import pages.BasePage;
+
+@RunWith(SerenityRunner.class)
+public class DefectsForHgvsAndTrailers_CVSB_3927 extends BasePage {
+}
