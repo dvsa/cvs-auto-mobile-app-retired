67d54f35fe4f3f5eb02689df978ea6db48d00a31
spwalsh <spwalsh@deloitte.co.uk>
2019-10-12 16:17:57 +0100

    First commit

--- src/test/java/testtype/CVSB_444/TestTypeLinked_CVSB_4680.java
+++ src/test/java/testtype/CVSB_444/TestTypeLinked_CVSB_4680.java
@@ -2,6 +2,8 @@ package testtype.CVSB_444;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.annotations.Title;
+import net.thucydides.core.annotations.WithTag;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import pages.TestPage;
@@ -40,6 +42,8 @@ public class TestTypeLinked_CVSB_4680 extends BaseTestClass {
     SiteVisitSteps siteVisitSteps;
 
     @Test
+    @Ignore
+    @WithTag("Smoke_1")
     @Title("CVSB-444 - TCD - AC1 As a VSA I would like to view only relevant linked tests so that I don't have to go through the full test type taxonomy (Voluntary Smoke Test)")
     public void testListFromAddLinkedTestTypeVoluntaryTestSmokeTest() {
         testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(), "Voluntary test");
