b74fe7e48b67f6a3283073ac5b6c8f2c1c571768
bflorea <bflorea@deloittece.com>
2019-12-05 10:11:10 +0000

    added in_test tag

--- src/test/java/vehicle/CVSB_3826/NewScreenDesignsTrl.java
+++ src/test/java/vehicle/CVSB_3826/NewScreenDesignsTrl.java
@@ -138,6 +138,7 @@ public class NewScreenDesignsTrl extends BaseTestClass {
         trailerDetailsSteps.checkBrakesIsDisplayed();
     }
 
+    @WithTag("In_test")
     @Title("CVSB-6190 - CVSB-6191 - AC 6.2 Vehicle not found in system (pop-up shown) (press OK)")
     @Test
     public void testNewDesignVehicleNotFound() {
