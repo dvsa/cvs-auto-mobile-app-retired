a0a9956df53dc5e37c84f4f856884114741437a1
dpanzaru@deloittece.com <dpanzaru@deloittece.com>
2020-02-20 12:51:56 +0200

    changes for deleting activities

--- src/main/java/util/backend/activity/ActivityService.java
+++ src/main/java/util/backend/activity/ActivityService.java
@@ -23,11 +23,33 @@ public class ActivityService {
     private static Response response;
 
     private void searchForTesterStaffId() {
+        response = activitiesClient.getAllActivities();
+        String testerName = getTesterName();
+        testerStaffId = null;
+        if (!response.getBody().asString().contains("No resources match the search criteria")) {
+            while (testerStaffId == null && !response.getBody().asString().contains("No resources match the search criteria")) {
+                testerStaffId = response.jsonPath().getString("find { it.testerName == '" + testerName + "'}.testerStaffId");
+                if (testerStaffId == null) {
+                    List<String> activityIds = response.jsonPath().getList("startTime");
+                    List<LocalDateTime> activityDateId = new ArrayList<>();
+                    for (String activityId : activityIds) {
+                        activityDateId.add(LocalDateTime.parse(activityId, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")));
+                    }
+                    Collections.sort(activityDateId);
+                    String toStartTimeData = activityDateId.get(0).minusSeconds(1).toString();
+                    response = activitiesClient.getActivitiesToStartTime(toStartTimeData);
+
+                }
+            }
+        }
+    }
+
+    public void closeUserActivity() {
         EnvironmentType envType = TypeLoader.getType();
 
         switch (envType) {
             case CI_BROWSERSTACK:
-                AwsUtil.deleteActivitiesForUser(System.getProperty("MICROSOFT_USERNAME"));
+                AwsUtil.deleteActivitiesForUser("a53ae740-eac4-4eb6-99ef-42afb63ce819");
                 break;
             case LOCAL_REAL_DEVICE:
             case LOCAL_SIMULATOR:
@@ -60,34 +82,6 @@ public class ActivityService {
                 throw new AutomationException("Environment configuration not found");
 
         }
-    }
-
-    public void closeUserActivity() {
-
-        if (testerStaffId == null) {
-            searchForTesterStaffId();
-        }
-
-        if (testerStaffId != null) {
-            response = activitiesClient.getActivities(testerStaffId);
-            if (!response.getBody().asString().contains("No resources match the search criteria")) {
-                if (response.getStatusCode() != 200) {
-                    throw new AutomationException("Response for get activities failed - Backend API Issue failed with status code "
-                            + response.getStatusCode() + " and body message " + response.getBody().asString());
-                }
-                List<String> activityIds = response.jsonPath().getList("findAll { it.endTime == null}.id");
-                if (activityIds != null) {
-                    for (String activityId : activityIds) {
-                        response = activitiesClient.putActivities(activityId);
-                        if (response.getStatusCode() != 204) {
-                            throw new AutomationException("Response for put activities failed - Backend API Issue failed with status code "
-                                    + response.getStatusCode() + " and body message " + response.getBody().asString());
-                        }
-                    }
-                }
-            }
-        }
-
     }
 
     private static String getTesterName() {
