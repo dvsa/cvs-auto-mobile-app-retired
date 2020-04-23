9966b0436f31544aa84a68de55ab380f50950dd5
dpanzaru@deloittece.com <dpanzaru@deloittece.com>
2020-04-23 14:29:32 +0300

    changes for running mobile tests in parallel

---
+++ src/main/java/util/AfterClassUtils.java
@@ -0,0 +1,76 @@
+package util;
+
+import exceptions.AutomationException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AfterClassUtils {
+
+    private static String userName = BaseUtils.getUserName();;
+    private static final String CI_ENV = "CI";
+    private static File mainFile = new File("userPool.txt");
+
+    public static void addUserBackToUserPool() {
+        if (TypeLoader.getType().getEnvType().equalsIgnoreCase(CI_ENV)) {
+
+            boolean fileExists = mainFile.exists();
+            if (!fileExists) {
+                throw new AutomationException("User pool file does not exist");
+            } else {
+                try {
+                    FileChannel fileChannel = new RandomAccessFile(mainFile, "rw").getChannel();
+                    FileLock lock = fileChannel.lock();
+                    while (!mainFile.canRead() || !mainFile.canWrite()) {
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    ByteBuffer bytes = ByteBuffer.allocate(500);
+                    int noOfBytesRead = fileChannel.read(bytes);
+                    StringBuilder users = new StringBuilder();
+
+                    Thread.sleep(2000);
+                    while (noOfBytesRead != -1) {
+                        bytes.flip();
+                        while (bytes.hasRemaining()) {
+                            users.append((char) bytes.get());
+                        }
+                        bytes.clear();
+                        noOfBytesRead = fileChannel.read(bytes);
+                    }
+
+                    fileChannel.truncate(0);
+                    List<String> myNewList = new ArrayList<>();
+                    if (users.length() != 0) {
+                        myNewList = new ArrayList<>(Arrays.asList(users.toString().split(",")));
+                    }
+                    myNewList.add(userName);
+                    String string2 = String.join(",", myNewList);
+                    fileChannel
+                            .write(Charset.defaultCharset().encode(CharBuffer.wrap(string2)));
+                    fileChannel.force(true);
+
+                    Thread.sleep(1000);
+
+                    lock.release();
+
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                    throw new AutomationException("Adding back users in user pool failed");
+                }
+            }
+        }
+    }
+}
--- src/main/java/util/backend/activity/ActivityService.java
+++ src/main/java/util/backend/activity/ActivityService.java
@@ -4,10 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import exceptions.AutomationException;
 import io.restassured.response.Response;
-import util.AwsUtil;
-import util.EnvironmentType;
-import util.TypeLoader;
-import util.WriterReader;
+import util.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -46,14 +43,49 @@ public class ActivityService {
 
     public void closeUserActivity() {
         EnvironmentType envType = TypeLoader.getType();
-
+        System.out.println("=========================ENV TYPE================================");
+        System.out.println(envType);
         switch (envType) {
             case CI_BROWSERSTACK:
-                AwsUtil.deleteActivitiesForUser("a53ae740-eac4-4eb6-99ef-42afb63ce819");
-                break;
+                System.out.println(envType);
+                System.out.println("======================= CURRENT USERNAME ==================================");
+                System.out.println(BaseUtils.getUserName());
+                switch(BaseUtils.getUserName()) {
+                    case "cvs.automation2@dvsagov.onmicrosoft.com":
+                        AwsUtil.deleteActivitiesForUser("a53ae740-eac4-4eb6-99ef-42afb63ce819");
+                        break;
+                    case "cvs.automation3@dvsagov.onmicrosoft.com":
+                        AwsUtil.deleteActivitiesForUser("d463f9f3-1ce7-4944-930a-ce9fd51e506b");
+                        break;
+                     case "cvs.automation4@dvsagov.onmicrosoft.com":
+                         AwsUtil.deleteActivitiesForUser("c26d6b02-49b2-456a-ac20-7435e9397bac");
+                        break;
+                     case "cvs.automation5@dvsagov.onmicrosoft.com":
+                         AwsUtil.deleteActivitiesForUser("4dd9756b-8a94-4cef-b58a-804a28ffbb68");
+                        break;
+                     case "cvs.automation6@dvsagov.onmicrosoft.com":
+                         AwsUtil.deleteActivitiesForUser("b358e472-007d-438c-9d0d-75f243a8cce6");
+                        break;
+                     case "cvs.automation7@dvsagov.onmicrosoft.com":
+                         AwsUtil.deleteActivitiesForUser("6719600b-3aaf-49ee-9efd-38031d49a167");
+                        break;
+                     case "cvs.automation8@dvsagov.onmicrosoft.com":
+                         AwsUtil.deleteActivitiesForUser("09bafb1b-9871-4848-a755-037755eb3d74");
+                        break;
+                     case "cvs.automation9@dvsagov.onmicrosoft.com":
+                         AwsUtil.deleteActivitiesForUser("5c4ed163-e54b-400f-ab9a-9d75c658c131");
+                        break;
+                    case "cvs.automation10@dvsagov.onmicrosoft.com":
+                        AwsUtil.deleteActivitiesForUser("8272a386-098b-4dc9-ab1d-c2ea561aa4a0");
+                        break;
+                    default:
+                        throw new AutomationException("The provided user is incorrect");
+                }
             case LOCAL_REAL_DEVICE:
             case LOCAL_SIMULATOR:
             case LOCAL_BROWSERSTACK:
+                System.out.println("======================= NOT CI===================================");
+                System.out.println(envType);
                 if (testerStaffId == null) {
                     searchForTesterStaffId();
                 }
