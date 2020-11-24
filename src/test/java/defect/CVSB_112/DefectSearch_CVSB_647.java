ccdbb26a18620aa0ef11af7070960809608e8ec1
deepikasingh <deepikasingh@deloitte.co.uk>
2020-11-24 13:05:58 +0000

    Defects - Test Case Fix

--- src/test/java/defect/CVSB_112/DefectSearch_CVSB_647.java
+++ src/test/java/defect/CVSB_112/DefectSearch_CVSB_647.java
@@ -27,19 +27,18 @@ public class DefectSearch_CVSB_647  extends BaseTestClass {
     DefectCategorySteps defectCategorySteps;
 
     @Steps
-    DefectItemSteps defectItemSteps;
-
-    @Steps
-    DefectDescriptionSteps defectDescriptionSteps;
-
-    @Steps
-    DefectDetailsSteps defectDetailsSteps;
+    TestTypeCategorySteps testTypeCategorySteps;
 
 
     @Title("CVSB-112 - AC4 non-alphanumeric search in Level 1 defect category search bar")
     @Test
     public void searchForNonAlphanumericDefect() {
-        testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        //testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(),
+        // preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testTypeCategoryComp.goToTestPage(preparerService.getPreparerByIndex(0).getPreparerId(),preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testSteps.addTestType();
+        testTypeCategorySteps.waitUntilPageIsLoaded();
+        testTypeCategorySteps.selectFromTestTypeList("Annual test");
         testSteps.selectTestType("Annual test", TestPage.TestTypeStatuses.IN_PROGRESS);
         testTypeDetailsSteps.selectAddDefect("Annual test");
         defectCategorySteps.checkListOfDefects("3. Seat Belts & Supplementary Restraint Systems", "Registration Plate");
