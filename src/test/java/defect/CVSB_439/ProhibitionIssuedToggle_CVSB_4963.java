a08c59e50fd27c67d706893a519d7eabf41f42fa
deepikasingh <deepikasingh@deloitte.co.uk>
2020-11-24 19:49:53 +0000

    test case fixed testProhibitionIssuedProhibitionClearance

--- src/test/java/defect/CVSB_439/ProhibitionIssuedToggle_CVSB_4963.java
+++ src/test/java/defect/CVSB_439/ProhibitionIssuedToggle_CVSB_4963.java
@@ -44,11 +44,16 @@ public class ProhibitionIssuedToggle_CVSB_4963 extends BaseTestClass {
     @Title("CVSB-439 - AC1 - VSA confirms a prohibition have been issued against a dangerous defect (Prohibition Clearance test)")
     @Test
     public void testProhibitionIssuedProhibitionClearance() {
-        testTypeCategoryComp.goToTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testTypeCategoryComp.goToTestPage(preparerService.getPreparerByIndex(0).getPreparerId(),preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testSteps.addTestType();
+        testTypeCategorySteps.waitUntilPageIsLoaded();
         testTypeCategorySteps.selectFromTestTypeList("Prohibition clearance");
         testTypeSubcategorySteps.selectFromTestTypeList("Any PSV");
+        testTypeSubcategorySteps.waitForPageToLoadBySubcategory("Any PSV");
         testTypeSubcategorySteps.selectFromTestTypeList("PG9 retest");
+        testTypeSubcategorySteps.waitForPageToLoadBySubcategory("PG9 retest");
         testTypeSubcategorySteps.selectFromTestTypeList("Paid");
+        testTypeSubcategorySteps.waitForPageToLoadBySubcategory("Paid");
         testTypeSubcategorySteps.selectFromTestTypeList("With certification");
         testSteps.selectTestType("Prohibition clearance", TestPage.TestTypeStatuses.IN_PROGRESS);
         testTypeDetailsSteps.selectAddDefect("Paid prohibition clearance (retest with certificate)");
