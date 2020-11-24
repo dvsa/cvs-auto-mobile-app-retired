397556f3059da7bf9150f8cbaa7e60d56eea1e76
deepikasingh <deepikasingh@deloitte.co.uk>
2020-11-24 17:24:55 +0000

    test case fixed testProhibitionIssuedAnnualTest

--- src/test/java/defect/CVSB_439/ProhibitionIssuedToggle_CVSB_4911.java
+++ src/test/java/defect/CVSB_439/ProhibitionIssuedToggle_CVSB_4911.java
@@ -34,10 +34,16 @@ public class ProhibitionIssuedToggle_CVSB_4911 extends BaseTestClass {
     @Steps
     DefectDetailsSteps defectDetailsSteps;
 
+    @Steps
+    TestTypeCategorySteps testTypeCategorySteps;
+
     @Title("CVSB-439 - AC1 - VSA confirms a prohibition have been issued against a dangerous defect (Annual Test)")
     @Test
     public void testProhibitionIssuedAnnualTest() {
-        testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testTypeCategoryComp.goToTestPage(preparerService.getPreparerByIndex(0).getPreparerId(),preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testSteps.addTestType();
+        testTypeCategorySteps.waitUntilPageIsLoaded();
+        testTypeCategorySteps.selectFromTestTypeList("Annual test");
         testSteps.selectTestType("Annual test", TestPage.TestTypeStatuses.IN_PROGRESS);
         testTypeDetailsSteps.selectAddDefect("Annual test");
         defectCategorySteps.selectDefectFromList("3. Seat Belts & Supplementary Restraint Systems");
