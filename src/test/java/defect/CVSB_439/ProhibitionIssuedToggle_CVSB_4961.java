51266ad9701841bd65e267093cf77cfe4a93a391
deepikasingh <deepikasingh@deloitte.co.uk>
2020-11-24 19:21:41 +0000

    test case fixed testProhibitionIssuedClass6A

--- src/test/java/defect/CVSB_439/ProhibitionIssuedToggle_CVSB_4961.java
+++ src/test/java/defect/CVSB_439/ProhibitionIssuedToggle_CVSB_4961.java
@@ -44,9 +44,12 @@ public class ProhibitionIssuedToggle_CVSB_4961 extends BaseTestClass {
     @Title("CVSB-439 - AC1 - VSA confirms a prohibition have been issued against a dangerous defect (Class 6A)")
     @Test
     public void testProhibitionIssuedClass6A() {
-        testTypeCategoryComp.goToTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testTypeCategoryComp.goToTestPage(preparerService.getPreparerByIndex(0).getPreparerId(),preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testSteps.addTestType();
+        testTypeCategorySteps.waitUntilPageIsLoaded();
         testTypeCategorySteps.selectFromTestTypeList("Class 6A");
         testTypeSubcategorySteps.selectFromTestTypeList("Annual test");
+        testSteps.waitUntilPageIsLoaded();
         testSteps.selectTestType("Class 6A", TestPage.TestTypeStatuses.IN_PROGRESS);
         testTypeDetailsSteps.selectAddDefect("Class 6A seatbelt installation check (annual test)");
         defectCategorySteps.selectDefectFromList("3. Seat Belts & Supplementary Restraint Systems");
