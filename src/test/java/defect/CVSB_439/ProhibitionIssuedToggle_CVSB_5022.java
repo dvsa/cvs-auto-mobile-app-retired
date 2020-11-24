396a53e3712f4744c2cbbe01d5bb4b606ba58e14
deepikasingh <deepikasingh@deloitte.co.uk>
2020-11-24 17:40:35 +0000

    test case fixed testProhibitionIssuedSwitchOff

--- src/test/java/defect/CVSB_439/ProhibitionIssuedToggle_CVSB_5022.java
+++ src/test/java/defect/CVSB_439/ProhibitionIssuedToggle_CVSB_5022.java
@@ -34,11 +34,16 @@ public class ProhibitionIssuedToggle_CVSB_5022 extends BaseTestClass {
     @Steps
     DefectDetailsSteps defectDetailsSteps;
 
+    @Steps
+    TestTypeCategorySteps testTypeCategorySteps;
 
     @Title("CVSB-439 - AC2 - VSA switches prohibition issued field off for a dangerous defect")
     @Test
     public void testProhibitionIssuedSwitchOff() {
-        testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testTypeCategoryComp.goToTestPage(preparerService.getPreparerByIndex(0).getPreparerId(),preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testSteps.addTestType();
+        testTypeCategorySteps.waitUntilPageIsLoaded();
+        testTypeCategorySteps.selectFromTestTypeList("Annual test");
         testSteps.selectTestType("Annual test", TestPage.TestTypeStatuses.IN_PROGRESS);
         testTypeDetailsSteps.selectAddDefect("Annual test");
         defectCategorySteps.selectDefectFromList("3. Seat Belts & Supplementary Restraint Systems");
