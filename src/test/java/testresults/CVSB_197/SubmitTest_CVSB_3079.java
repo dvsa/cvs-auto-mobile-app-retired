11332e766bedba59eb5450202c7e010f7f54751a
Tanio Artino <tartino@.deloitte.co.uk>
2020-11-24 12:19:49 +0000

    fixed test SubmitTest_CVSB_3079.java
    Signed-off-by: Tanio Artino <tartino@.deloitte.co.uk>

--- src/test/java/testresults/CVSB_197/SubmitTest_CVSB_3079.java
+++ src/test/java/testresults/CVSB_197/SubmitTest_CVSB_3079.java
@@ -56,6 +56,7 @@ public class SubmitTest_CVSB_3079 extends BaseTestClass {
         odometerReadingSteps.checkReadingValue("123");
         odometerReadingSteps.pressSave();
         testSteps.addTestType();
+        testTypeCategorySteps.waitUntilPageIsLoaded();
         testTypeCategorySteps.selectFromTestTypeList("Annual test");
         testSteps.selectTestType("Annual test", TestPage.TestTypeStatuses.IN_PROGRESS);
         testTypeDetailsSteps.setCarriedOutDuringTest(true);
