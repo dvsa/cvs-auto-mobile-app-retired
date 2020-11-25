01c7d701e7cb31097f14f7877b1f7899ed54b391
Zac Mohamoud <zmohamoud@deloitte.co.uk>
2020-11-25 15:45:05 +0000

    test case fixed testTextBoxForAdditionalComments

--- src/main/java/pages/TestPage.java
+++ src/main/java/pages/TestPage.java
@@ -21,7 +21,7 @@ public class TestPage extends BasePage {
     private static final String REMOVE_ID = "Remove";
     private static final String DESCRIPTION_ID = "This action will remove this test from the vehicle.";
     private static final String TITLE_ID = "Remove test";
-    private static final String ABANDON_BUTTON_ID = "Abandon";
+    private static final String ABANDON_BUTTON_ID = "Abandon test type";
     private static final String TEST_NOT_COMPLETE_TITLE = "Test not complete";
     private static final String TEST_NOT_COMPLETE_INFO = "You must complete all test types marked \"in progress\" before reviewing.";
     private static final String OK_BUTTON = "OK";
