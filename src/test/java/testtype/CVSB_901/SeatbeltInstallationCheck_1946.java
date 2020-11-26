62e228f1452c056ccc5de677cb3ba9f6b7b0dd88
Zac Mohamoud <zmohamoud@deloitte.co.uk>
2020-11-26 14:04:36 +0000

    fix tests for testEditNumberOfSeatbelts

--- src/test/java/testtype/CVSB_901/SeatbeltInstallationCheck_1946.java
+++ src/test/java/testtype/CVSB_901/SeatbeltInstallationCheck_1946.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import pages.TestPage;
 import steps.SeatbeltInstallationCheckSteps;
 import steps.TestSteps;
+import steps.TestTypeCategorySteps;
 import steps.TestTypeDetailsSteps;
 import steps.composed.TestTypeCategoryComp;
 import steps.util.UtilSteps;
@@ -31,17 +32,25 @@ public class SeatbeltInstallationCheck_1946 extends BaseTestClass {
     @Steps
     SeatbeltInstallationCheckSteps seatbeltInstallationCheckSteps;
 
+    @Steps
+    TestTypeCategorySteps testTypeCategorySteps;
+
     @Title("CVSB-901 - AC4 - Edit number of seat belts fitted")
     @Test
     public void testEditNumberOfSeatbelts() {
         utilSteps.showBrowserstackUrl(super.sessionDetails.getBsSessionUrl());
-        testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testTypeCategoryComp.goToTestPage(preparerService.getPreparerByIndex(0).getPreparerId(),preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testSteps.addTestType();
+        testTypeCategorySteps.waitUntilPageIsLoaded();
+        testTypeCategorySteps.selectFromTestTypeList("Annual test");
         testSteps.selectTestType("Annual test", TestPage.TestTypeStatuses.IN_PROGRESS);
         testTypeDetailsSteps.selectNumberOfSeatbeltsFitted();
         seatbeltInstallationCheckSteps.inputNumberOfSeatbelts("4");
+        testTypeDetailsSteps.waitUntilPageIsLoaded();
         testTypeDetailsSteps.checkNumberOfSeatbelts("4");
         testTypeDetailsSteps.selectNumberOfSeatbeltsFitted();
         seatbeltInstallationCheckSteps.inputNumberOfSeatbelts("5");
+        testTypeDetailsSteps.waitUntilPageIsLoaded();
         testTypeDetailsSteps.checkNumberOfSeatbelts("5");
     }
 }
