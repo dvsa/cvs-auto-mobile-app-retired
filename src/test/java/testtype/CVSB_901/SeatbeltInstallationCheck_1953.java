0e943b3814bcb6b4561c953a98d486b5c728387b
Zac Mohamoud <zmohamoud@deloitte.co.uk>
2020-11-26 14:12:29 +0000

    fix tests for testCancellationOfMostRecentInstallationCheck

--- src/test/java/testtype/CVSB_901/SeatbeltInstallationCheck_1953.java
+++ src/test/java/testtype/CVSB_901/SeatbeltInstallationCheck_1953.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import pages.TestPage;
 import steps.TestSteps;
+import steps.TestTypeCategorySteps;
 import steps.TestTypeDetailsSteps;
 import steps.composed.TestTypeCategoryComp;
 import steps.util.UtilSteps;
@@ -27,11 +28,17 @@ public class SeatbeltInstallationCheck_1953 extends BaseTestClass {
     @Steps
     TestTypeDetailsSteps testTypeDetailsSteps;
 
+    @Steps
+    TestTypeCategorySteps testTypeCategorySteps;
+
     @Title("CVSB-901 - AC11 - Cancel selection of most recent installation check")
     @Test
     public void testCancellationOfMostRecentInstallationCheck() {
         utilSteps.showBrowserstackUrl(super.sessionDetails.getBsSessionUrl());
-        testTypeCategoryComp.completeAddTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testTypeCategoryComp.goToTestPage(preparerService.getPreparerByIndex(0).getPreparerId(),preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testSteps.addTestType();
+        testTypeCategorySteps.waitUntilPageIsLoaded();
+        testTypeCategorySteps.selectFromTestTypeList("Annual test");
         testSteps.selectTestType("Annual test", TestPage.TestTypeStatuses.IN_PROGRESS);
         testTypeDetailsSteps.selectMostRecentInstallationCheck();
         testTypeDetailsSteps.setMostRecentInstallationCheckDateOneUnit();
