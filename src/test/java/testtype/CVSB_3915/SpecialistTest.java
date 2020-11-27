bf0a7b33f8b24afff3494fae69a78b4a88998388
deepikasingh <deepikasingh@deloitte.co.uk>
2020-11-27 16:43:04 +0000

    test fixed testDefectAttributes

--- src/test/java/testtype/CVSB_3915/SpecialistTest.java
+++ src/test/java/testtype/CVSB_3915/SpecialistTest.java
@@ -488,7 +488,11 @@ public class SpecialistTest extends BaseTestClass {
     @Test
     public void testDefectAttributes() {
         utilSteps.showBrowserstackUrl(super.sessionDetails.getBsSessionUrl());
-        vehicleComp.goToVehicleDetails("1B7GG36N12S678410", super.username); // PSV
+        vehicleComp.goToIdentifyVehicle(super.username);
+        identifyVehicleSteps.searchForVehicle("1B7GG36N12S678410");// PSV
+        identifyVehicleSteps.waitUntilPageIsLoaded();
+        identifyVehicleSteps.clickSearchCriteriaButton();
+        vehicleDetailsSteps.waitUntilPageIsLoaded();
         vehicleDetailsSteps.selectConfirmButtonTopRight();
         vehicleDetailsSteps.selectConfirmFromPopUp();
         preparerSteps.startTest();
