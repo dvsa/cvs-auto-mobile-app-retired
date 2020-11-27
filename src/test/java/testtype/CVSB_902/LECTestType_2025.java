3fd5228aabab678602aca78f03b08ee802941547
deepikasingh <deepikasingh@deloitte.co.uk>
2020-11-27 12:49:25 +0000

    test fixed passLecTestType

--- src/test/java/testtype/CVSB_902/LECTestType_2025.java
+++ src/test/java/testtype/CVSB_902/LECTestType_2025.java
@@ -9,6 +9,7 @@ import pages.TestPage;
 import steps.TestSteps;
 import steps.TestTypeCategorySteps;
 import steps.TestTypeDetailsSteps;
+import steps.TestTypeSubcategorySteps;
 import steps.composed.TestTypeCategoryComp;
 import steps.util.UtilSteps;
 import utils.BaseTestClass;
@@ -31,13 +32,19 @@ public class LECTestType_2025 extends BaseTestClass {
     @Steps
     TestTypeDetailsSteps testTypeDetailsSteps;
 
+    @Steps
+    TestTypeSubcategorySteps testTypeSubcategorySteps;
+
     @Title("CVSB-902 - AC2 - Select results for test types - LEC - Pass")
     @Test
     public void passLecTestType() {
         utilSteps.showBrowserstackUrl(super.sessionDetails.getBsSessionUrl());
         testTypeCategoryComp.goToTestType(preparerService.getPreparerByIndex(0).getPreparerId(), preparerService.getPreparerByIndex(0).getPreparerName(),super.username);
+        testTypeCategorySteps.waitUntilPageIsLoaded();
         testTypeCategorySteps.selectFromTestTypeList("Technical test");
+        testTypeSubcategorySteps.waitForPageToLoadBySubcategory("Technical test");
         testTypeCategorySteps.selectFromTestTypeList("LEC");
+        testSteps.waitUntilPageIsLoaded();
         testSteps.selectTestType("Technical test", TestPage.TestTypeStatuses.IN_PROGRESS);
         testTypeDetailsSteps.setTestToOption("Pass");
         testTypeDetailsSteps.checkTestIsPassed();
