fbac396fb29e06ee1c4d22df130a6ab6e54b9d15
iulia <iulia.butuza2dvsa.gov.uk>
2021-08-31 15:42:14 +0100

    only left my tag

--- src/test/java/testtype/CVSB_15130/IVATestUpdated_CVSB_15130.java
+++ src/test/java/testtype/CVSB_15130/IVATestUpdated_CVSB_15130.java
@@ -150,6 +150,7 @@ public class IVATestUpdated_CVSB_15130 extends BaseTestClass {
         testTypeDetailsSteps.checkCertificateNumberIs("1111111111ABCDEFGHIJ");
     }
 
+    @WithTag("In_Test")
     @Title("CVSB-15130 - AC1 - Certificate number for Failures - PSV - YEL")
     @Test
     public void testIVACertificateNumber_Failures_PSV_YEL() {
